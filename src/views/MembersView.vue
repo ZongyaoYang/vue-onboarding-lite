138490a9ae31334df16e929e28cd355d89bd842d
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-21 23:03:07 -0600

    add member view

---
+++ src/views/MembersView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+
+const auth = useAuthStore();
+const router = useRouter();
+
+function logout() {
+    auth.logout()
+    router.push({ name: 'login'})
+}
+
+</script>
+
+<template>
+    <header class="bar">
+        <span>Singed in as {{ auth.email }}</span>
+        <button type="button" @click="logout">Log out</button>
+    </header>
+    <h1>Members (placeholder)</h1>
+</template>
