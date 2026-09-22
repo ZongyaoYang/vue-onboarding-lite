9e3bea65f81ca2826a7e1d2656702a904f7a07aa
ZongyaoYang <zongyaoyang@gmail.com>
2026-09-21 20:17:51 -0600

    add router and route guard

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/members' },
+    { path: 'login', name: 'login', component: () => import('@/views/LoginView.vue') },
+    {
+      path: 'members',
+      name: 'members',
+      component: () => import("@/views/LoginView.vue"),
+      meta: { requiresAuth: true },
+    }
+  ],
+})
+
+router.beforeEach(to => {
+  const auth = useAuthStore();
+  if (to.meta.requiresAuth && !auth.isLoggedIn) {
+    return { name: 'login', query: { redirect: to.fullPath } }
+  }
 })
 
 export default router
